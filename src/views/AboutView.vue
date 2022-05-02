00484d16b2875724d88dd601f9b26bd4283d42aa
simamkele21 <simamkelejanuary@gmail.com>
2022-05-02 02:42:41 +0200

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, sit ab vel harum tenetur voluptatum eos aspernatur itaque, vitae, consequuntur doloremque architecto! Perferendis eos laudantium facilis, unde labore esse doloremque.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, sit ab vel harum tenetur voluptatum eos aspernatur itaque, vitae, consequuntur doloremque architecto! Perferendis eos laudantium facilis, unde labore esse doloremque.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, sit ab vel harum tenetur voluptatum eos aspernatur itaque, vitae, consequuntur doloremque architecto! Perferendis eos laudantium facilis, unde labore esse doloremque.</p>
   </div>
 </template>
