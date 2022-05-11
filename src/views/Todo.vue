09f88e60a02c008608f3b69da0dd758bc0032d44
simamkele21 <simamkelejanuary@gmail.com>
2022-05-11 20:49:06 +0200

    /

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -53,9 +53,66 @@
 </template>
 
 <script>
-    const STORAGE_KEY = 'vue-todo-app-storage';
-  export default {
-    name: 'Home'
+  // new Vue({
+    //   el:".todoapp",
+  //   data() {
+    //     return {
+      //       tasks: [],
+  //       newTask: "",
+  //       editedTask: null,
+  //       editedTaskText: null,
+  //     }
+  //   },
+  //   created() {
+  //     this.tasks = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+  //   },
+  //   methods: {
+  //     addTask() {
+  //       this.tasks.push({
+  //         id:3,
+  //         taskCompletionToggleButtonState: "",
+  //         taskCompletionState:"incomplete",
+  //         text: this.newTask
+  //       })
+  //       this.newTask = "";
+        
+  //       localStorage.setItem(STORAGE_KEY, JSON.stringify(this.tasks));
+  //   }
+  // },
+  // deleteTask(task) {
+  //   this.tasks.splice(this.indexof(task), 1);
+  //   localStorage.setItem(STORAGE_KEY, JSON.stringify(this.tasks));
+  // },
+  // editTask(task) {
+    //   this.editedTask = task;
+  //   this.editedTaskText = task.text;
+  // },
+  // updatedTask(task) {
+    //   if(!this.editedTask) {
+      //     return;
+  //   } else if( !task.text) {
+  //     this.deleteTask(task);
+  //   }
+
+  //   this.editedTodo  = null;
+  //   task.text = this.editedTaskText.trim();
+
+  //   localStorage.setItem(STORAGE_KEY, JSON.stringify(this.tasks));
+  // },
+  // toggleTaskCompletionState(task) {
+  //   if (task.taskCompletionStateToggleButtonState === "" ||  task.taskCompletionStateToggleButtonState === false) {
+    //     task.taskCompletionStateToggleButtonState == "checked";
+  //     task.taskCompletionState = "completed";
+  //   }else if (task.taskCompletionStateToggleButtonState = "checked" || 
+  //   task.taskCompletionStateToggleButtonState === true) {
+  //     task.taskCompletionStateToggleButtonState = "checked";
+  //     task.taskCompletionState = "incomplete";
+  //   }
+  // }
+  // })
+STORAGE_KEY = 'vue-todo-app-storage';
+export default {
+  name: 'Home',
     data() {
       return {
         newTaskTitle: "",
@@ -74,16 +131,17 @@
           done: false
         }
         this.tasks.push(newTask)
-        this.newTaskTitle = ''
-        localStorage.setItem('value', this.value);
-        console.log(localStorage.getItem('value'))
+        this.newTaskTitle = ''  
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(this.tasks));
       },
       doneTask(id) {
         let task = this.tasks.filter( task => task.id === id) [0]
         task.done = !task.done
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(this.tasks));
       },
       deleteTask(id) {
         this.tasks = this.tasks.filter(task => task.id !== id)
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(this.tasks));
       }
     }
   }
